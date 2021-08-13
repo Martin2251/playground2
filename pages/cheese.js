7821cd6f7d6f7ed08b724c3277f71758a11a6c5b
martin lowe <martinlowe673@gmail.com>
2021-08-13 06:16:18 +0100

    added boiler plate and cheese page

---
+++ pages/cheese.js
@@ -0,0 +1,11 @@
+export const cheese = () => {
+  return (
+    <div className="page-container">
+      <div>
+        <h1>Cheese of the World </h1>
+      </div>
+    </div>
+  );
+};
+
+export default cheese;
