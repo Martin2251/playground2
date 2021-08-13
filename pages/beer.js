e5018d19b1f45fd1ff125b338dcc5cbf7a5e2b35
martin lowe <martinlowe673@gmail.com>
2021-08-13 08:54:44 +0100

    added heading and toolbar to beer page

--- pages/beer.js
+++ pages/beer.js
@@ -1,18 +1,21 @@
+import styled from "styled-components";
+import { Toolbar } from "../components/toolbar";
+
 export const beer = () => {
   return (
     <div className="page-container">
-      <Heading>Cheese of the World </Heading>
-      <div>
-        <h3>Brie</h3>
-      </div>
-      <div>
-        <h3>Cheddar</h3>
-      </div>
-      <div>
-        <Red>Red Lecister</Red>
-      </div>
+      <Toolbar></Toolbar>
+      <Heading>Beer of the World </Heading>
+
+      <div></div>
     </div>
   );
 };
 
 export default beer;
+
+const Heading = styled.h1`
+  font-size: 5em;
+  text-align: center;
+  color: red;
+`;
