50d03dd9124d1ba8a9fb7678da0b971845c0df76
martin lowe <martinlowe673@gmail.com>
2021-08-13 08:22:10 +0100

    styled home page

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,31 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { Toolbar } from "../components/toolbar";
+import styled from "styled-components";
 
 export default function Home() {
   return (
     <div>
       <Toolbar></Toolbar>
+      <MainHeader>The SHop</MainHeader>
 
-      <h1>The SHop</h1>
+      <Section>
+        <p>
+          Welcome to the local shop we are very posh, we sell high quality
+          cheese,wine and Beer
+        </p>
+      </Section>
     </div>
   );
 }
+
+const MainHeader = styled.h1`
+  font-size: 5em;
+  text-align: center;
+  color: red;
+`;
+const Section = styled.p`
+  font-size: 2em;
+  text-align: center;
+  color: red;
+`;
