18ad781c3dc123e5270069bc594db8a28c9c3f40
martin lowe <martinlowe673@gmail.com>
2021-08-14 16:33:08 +0100

    added hard-coded data

--- pages/beer.js
+++ pages/beer.js
@@ -1,14 +1,25 @@
 import styled from "styled-components";
 import { Toolbar } from "../components/toolbar";
+import Image from "next/image";
+import styles from "../styles/Beer.module.css";
 
 export const beer = () => {
   return (
-    <div className="page-container">
+    <>
       <Toolbar></Toolbar>
       <Heading>Beer of the World </Heading>
-
-      <div></div>
-    </div>
+      <section className="products">
+        <div className="product-card">
+          <div className="product-image">
+            <image src="../public/stella.jpg"></image>
+          </div>
+          <div className="product-info">
+            <h5>Stella</h5>
+            <h6>£6.00</h6>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
